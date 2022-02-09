492666ed5ff014f57b58d78b499640e1f26df362
Ayman292 <aymanelbanna1011@gmail.com>
2022-02-09 16:22:37 -0500

    Homework

---
+++ My project/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public int moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Time.deltaTime * moveSpeed * Vector3.right; 
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Time.deltaTime * moveSpeed * Vector3.left; 
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Time.deltaTime * moveSpeed * Vector3.forward; 
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Time.deltaTime * moveSpeed * Vector3.back; 
+        }
+    }
+}
